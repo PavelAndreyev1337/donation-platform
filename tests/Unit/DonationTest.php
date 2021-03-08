8ccc2f7a94112f1cdab7fca501bc729aa2352ce1
Pavel_Andreyev <pavel.andreyev.98@gmail.com>
2021-03-08 22:20:41 +0200

    added instantiation test for donation model

--- tests/Unit/DonationTest.php
+++ tests/Unit/DonationTest.php
@@ -2,33 +2,19 @@
 
 namespace Tests\Unit;
 
+use App\Models\Donation;
 use Tests\TestCase;
 
 class DonationTest extends TestCase
 {
     /**
-     * Test donation creation.
+     * Test donation can be instantiated.
      *
      * @return void
      */
-    public function testCanCreateDonation()
+    public function test_donation_can_be_instantiated()
     {
-        $data = [
-            'name' => $this->faker->name,
-            'email' => $this->faker->safeEmail,
-            'amount' => rand(0, 100),
-            'message' => $this->faker->sentence(5)
-        ];
-        $this->post(route('donations.store'), $data)->assertStatus(201);
-    }
-
-    /**
-     * Test donation outputting.
-     *
-     * @return void
-     */
-    public function testCanListPost()
-    {
-        $this->get(route('donations.index'))->assertStatus(200);
+       $donation = Donation::factory()->create();
+       $this->assertDatabaseHas("donations", $donation->toArray());
     }
 }
