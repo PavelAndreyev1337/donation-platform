b69e2c89583f53f750f2079ef916bc21b14a7781
Pavel_Andreyev <pavel.andreyev.98@gmail.com>
2021-03-10 23:29:42 +0200

    added unit tests for donation repository

--- tests/Unit/DonationRepositoryTest.php
+++ tests/Unit/DonationRepositoryTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Contracts\DonationRepositoryInterface;
 use App\Models\Donation;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -21,9 +22,8 @@ class DonationRepositoryTest extends TestCase
     /** @test */
     public function test_all()
     {
-        Donation::factory()->count(3)->create();
-        $this->assertDatabaseHas(
-            $this->table,
+        $this->assertEquals(
+            Donation::factory()->count(3)->create()->toArray(),
             $this->repository->all()->toArray()
         );
     }
@@ -69,9 +69,72 @@ class DonationRepositoryTest extends TestCase
             $donation->id,
             $this->repository->show($donation->id)->id
         );
-        $this->assertDatabaseHas(
-            $this->table,
-            $donation->toArray()
+    }
+
+    /** @test */
+    public function test_get_top_donator()
+    {
+        $maxDonation = Donation::factory()->count(10)->create()->max('amount');
+        $this->assertEquals(
+            $maxDonation,
+            $this->repository->getTopDonator()->amount
+        );
+    }
+
+    /** @test */
+    public function test_get_day_amount()
+    {
+        $today = Carbon::now()->today();
+        $this->assertEquals(
+            Donation::factory()->count(20)->create(
+                [
+                    'created_at' => $today,
+                    'updated_at' => $today
+                ]
+            )->sum('amount'),
+            $this->repository->getDayAmount()
+        );
+    }
+
+    /** @test */
+    public function test_get_month_amount()
+    {
+        $today = Carbon::now()->month();
+        $this->assertEquals(
+            Donation::factory()->count(20)->create(
+                [
+                    'created_at' => $today,
+                    'updated_at' => $today
+                ]
+            )->sum('amount'),
+            $this->repository->getMonthAmount()
+        );
+    }
+
+    /** @test */
+    public function test_get_amount_by_day()
+    {
+        collect([now(), now()->addDay()])->each(function ($item) {
+            $donations = Donation::factory()->count(5)->create(
+                [
+                    'created_at' => $item,
+                    'updated_at' => $item,
+                ]
+            );
+            $this->assertEquals(
+                $donations->sum('amount'),
+                $this->repository->getAmountByDay()->last()
+            );
+        });
+    }
+
+    /** @test */
+    public function test_paginate_ordered_donations()
+    {
+        Donation::factory()->count(10)->create();
+        $this->assertEquals(
+            10,
+            $this->repository->paginateOrderedDonations(1)->count()
         );
     }
 }
